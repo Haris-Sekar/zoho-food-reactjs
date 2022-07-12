d7d3a900cc51a01e1878837294e4a4904bbeaae5
Haris Sekar <sharis14003@gmail.com>
2022-07-12 13:48:54 +0530

    Auth added

--- src/main/webapp/Components/Auth/Auth.js
+++ src/main/webapp/Components/Auth/Auth.js
@@ -1,3 +1,132 @@
 function Auth(props) {
-    
+  console.log(props);
+  if (props.type === "login")
+    return (
+      <div className="loginContainer">
+        <div className="login">
+          <form id="loginForm" method="post">
+            <div id="text">Login</div>
+            <input
+              type="text"
+              name="email"
+              id=""
+              placeholder="Email address"
+              autoComplete="off"
+            />
+            <br />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+            <br />
+            <input type="checkbox" name="showPassword" id="showPassword" />
+            <span id="showpassword">Show Password</span>
+            <br />
+            <input type="button" value="Login"  />
+            <br />
+            <a href="" id="forgetPass">
+              Forget password?
+            </a>{" "}
+            <br />
+            <div id="signup">
+              New to Zoho Foods?{" "}
+              <a href="./signup.html" id="forgetPass">
+                Signup
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  else if (props.type === "register")
+    return (
+      <div className="signupContainer">
+        <div className="signup">
+          <form method="post" id="registerForm">
+            <div id="text">Signup</div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              autoComplete="off"
+              id="name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+            <input
+              type="number"
+              name="phone"
+              id="phone"
+              placeholder="Mobile number"
+              autoComplete="off"
+              required
+            />
+            <input
+              type="text"
+              name="address"
+              id="address"
+              placeholder="Address"
+              autoComplete="off"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              id="signupPassword"
+              placeholder="Password"
+              required
+            />
+            <input
+              type="password"
+              name="cpassword"  
+              id="cpassword"
+              placeholder="Confirm Password"
+            //   onFocus ="passwordCheck()"
+              required
+            />
+            <br />
+            <input type="file" name="file" id="file" />
+            <div id="err">
+              <ul>
+                <li>
+                  <span>
+                    <img
+                      src="./assets/images/remove.png"
+                      alt="asdf" 
+                      id="errImg6"
+                    />
+                  </span>
+                  Password must be atleast 6 characters
+                </li>
+              </ul>
+            </div>
+            <input
+              type="button"
+              value="signup"
+              id="submit"
+            />
+            <br />
+            <a href="" id="forgetPass">
+              Forget password?
+            </a>{" "}
+            <br />
+            <div id="signup">
+              Already have an account in Zoho Foods?{" "}
+              <a href="./login.html" id="forgetPass">
+                Login
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
 }
--- src/main/webapp/Components/Navbar/Navbar.js
+++ src/main/webapp/Components/Navbar/Navbar.js
@@ -1,23 +1,62 @@
 function Navbar() {
-  const userDet =userAuth(); 
-  return (
-    <div className="navbar">
-      <div className="logo">
-        <a href="">
-          <img src="./assets/images/logo.png" alt="" />
-        </a>
+  const Link = ReactRouterDOM.Link;
+  const userDet = userAuth();
+  if (userDet.type === "user")
+    return (
+      <div class="navbar">
+        <div class="logo">
+          <a href="../">
+            <img src="../assets/images/logo.png" alt="" />
+          </a>
+        </div>
+        <div class="navContent">
+          <a href="./restaurant.html" class="navConText">
+            Restaurants
+          </a>
+          <a href="./orders.html" class="navConText">
+            Orders
+          </a>
+          <a href="./cart.html" class="navConText">
+            <div class="navCart">
+              <div class="navCart-icon">
+                <img src="../assets/images/cart.png" alt="" />
+              </div>
+              <div class="navCart-badge">
+                <span id="cartNo">0</span>
+              </div>
+            </div>
+          </a>
+          <div class="profilePic" id="profilePic">
+            <div class="imgCon" id="imgConProfile"></div>
+            <div class="dropDown" id="dropDown">
+              <a href="./account.html" class="navConText1">
+                Account
+              </a>
+              <div class="navConText1" onclick="logout()">
+                Logout
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="navContent">
-        <a href="./template/restaurant/login.html" className="navConText">
-          Restaurant Login
-        </a>
-        <a href="./template/login.html">
-          <button className="btnnavbar">Login</button>
-        </a>
-        <a href="./template/signup.html">
-          <button className="btnnavbar">Signup</button>
-        </a>
+    );
+  else
+    return (
+      <div className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <img src="./assets/images/logo.png" alt="" />
+          </Link>
+        </div>
+        <div className="navContent">
+          <Link to="/auth/restaurantLogin" className="navConText">Restaurant Login</Link>
+          <Link to="/auth/login">
+            <button className="btnnavbar">Login</button>
+          </Link>
+          <Link to="/auth/register">
+            <button className="btnnavbar">Signup</button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
